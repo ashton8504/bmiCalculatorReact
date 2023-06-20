ed1b5488e3473f19daacc20c3a4685960661f658
ashton8504 <ashtonlittle8504@gmail.com>
2023-06-20 13:56:14 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import BMICalculator from "./Components/BMICalculator"
-import './index.css';
+import React, { useState } from "react";
+import BMICalculator from "./Components/BMICalculator";
+
+import "./index.css";
 
 function App() {
+  const [bmi, setBMI] = useState(null);
+
+  const calculateBMI = (calculatedBMI) => {
+    setBMI(calculatedBMI);
+  };
+
   return (
     <div className="container">
       <div className="center-container">
         <h1 className="header--title">React BMI Calculator</h1>
-        <BMICalculator />
+        <BMICalculator onCalculate={calculateBMI} />
+    
       </div>
     </div>
   );
--- src/Components/BMICalculator.js
+++ src/Components/BMICalculator.js
@@ -1,69 +1,69 @@
 import React, { useState } from "react";
 
-export default function BMICalculator() {
-  const [heightFeet, setHeightFeet] = useState(0);
-  const [heightInches, setHeightInches] = useState(0);
-  const [weightPounds, setWeightPounds] = useState(0);
+export default function BMICalculator({ heightFeet, heightInches, weightPounds }) {
+  const [feet, setFeet] = useState(heightFeet || 0);
+  const [inches, setInches] = useState(heightInches || 0);
+  const [pounds, setPounds] = useState(weightPounds || 0);
+  const [bmi, setBMI] = useState(null); // State to store the calculated BMI
 
   const calculateBMI = () => {
-    const totalHeightInches = parseInt(heightFeet) * 12 + parseInt(heightInches);
-    const weightInKg = parseInt(weightPounds) * 0.453592; // Convert pounds to kilograms
+    const totalHeightInches = parseInt(feet) * 12 + parseInt(inches);
+    const weightInKg = parseInt(pounds) * 0.453592; // Convert pounds to kilograms
     const heightInMeters = totalHeightInches * 0.0254; // Convert inches to meters
-  
-    const bmi = weightInKg / (heightInMeters * heightInMeters);
-    // Perform any additional logic or actions with the calculated BMI value
-    console.log("BMI:", bmi);
+
+    const calculatedBMI = weightInKg / (heightInMeters * heightInMeters);
+    setBMI(calculatedBMI); // Update the state with the calculated BMI
   };
-  
-  
+
   return (
-      <div className="bmi-form">
-          {/* Feet Section */}
+    <div className="bmi-form">
       <h2 className="input-header">Enter your height:</h2>
-            <div className="input-container">
-                <label className="input-label">Feet:</label>
-                <input
-                className="height-feet-input"
-                type="number"
-                placeholder="Feet"
-                value={heightFeet}
-                onChange={(e) => setHeightFeet(e.target.value)}
-                name="height-feet"
-                id="height-feet-input"
-                />
-          </div>
-          {/* Inches Section */}
       <div className="input-container">
-            <label className="input-label">Inches:</label>
-            <input
-                className="height-inches-input"
-                type="number"
-                placeholder="Inches"
-                value={heightInches}
-                onChange={(e) => setHeightInches(e.target.value)}
-                name="height-inches"
-                id="height-inches-input"
-            />
-          </div>
-          {/* Weight Section */}
+        <label className="input-label">Feet:</label>
+        <input
+          className="height-feet-input"
+          type="number"
+          placeholder="Feet"
+          value={feet}
+          onChange={(e) => setFeet(e.target.value)}
+          name="height-feet"
+          id="height-feet-input"
+        />
+      </div>
+      <div className="input-container">
+        <label className="input-label">Inches:</label>
+        <input
+          className="height-inches-input"
+          type="number"
+          placeholder="Inches"
+          value={inches}
+          onChange={(e) => setInches(e.target.value)}
+          name="height-inches"
+          id="height-inches-input"
+        />
+      </div>
       <h2 className="input-header">Enter your weight:</h2>
       <div className="input-container">
-            <label className="input-label">Pounds:</label>
-            <input
-                className="weight-pounds-input"
-                type="number"
-                placeholder="Pounds"
-                value={weightPounds}
-                onChange={(e) => setWeightPounds(e.target.value)}
-                name="weight-pounds"
-                id="weight-pounds-input"
-            />
-        </div>
-          
+        <label className="input-label">Pounds:</label>
+        <input
+          className="weight-pounds-input"
+          type="number"
+          placeholder="Pounds"
+          value={pounds}
+          onChange={(e) => setPounds(e.target.value)}
+          name="weight-pounds"
+          id="weight-pounds-input"
+        />
+      </div>
       <button className="calculate-button" onClick={calculateBMI}>
         Calculate BMI
-        </button>
-          
+      </button>
+      {bmi !== null && (
+        <div className="bmi-result">
+          <h2>BMI Result:</h2>
+          <p>{bmi.toFixed(2)}</p>
+        </div>
+      )}
     </div>
   );
 }
